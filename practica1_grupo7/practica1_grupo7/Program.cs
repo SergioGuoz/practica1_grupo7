33a884acff5acf37da99884e8ff35048f4566258
Dannek Miranda <mdannek@outlook.com>
2019-08-26 23:11:32 -0600

    Potencia al Cubo

--- practica1_grupo7/practica1_grupo7/Program.cs
+++ practica1_grupo7/practica1_grupo7/Program.cs
@@ -11,8 +11,9 @@ namespace practica1_grupo7
         static void Main(string[] args)
         {
             //numero_Primo();
-            Discriminante();
-            ValorAbsoluto();
+            //Discriminante();
+            //ValorAbsoluto();
+            Potencia_al_cubo();
         }
 
         static void numero_Primo()
@@ -71,6 +72,18 @@ namespace practica1_grupo7
             Console.WriteLine("Porfavor Ingresar un numero");
             numero = int.Parse(Console.ReadLine());
             Console.WriteLine("El valor absulito de ({0}) es {1} ", numero, Math.Abs(numero));
+
+            //Esto es para poder ver el resultado y no se cierre la consola
+            Console.ReadKey();
+        }
+
+        static void Potencia_al_cubo()
+        {
+            double numero;
+
+            Console.WriteLine("Porfavor Ingresar un numero");
+            numero = double.Parse(Console.ReadLine());
+            Console.WriteLine("El cubo de ({0}) es {1} ", numero, Math.Pow(numero,3));
 
             //Esto es para poder ver el resultado y no se cierre la consola
             Console.ReadKey();
